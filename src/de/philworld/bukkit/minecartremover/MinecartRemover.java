9f6849594ebdb0454e0c83f0c003d9737d0c4b9a
Philipp Schroer <hebipp1@googlemail.com>
2012-01-22 11:29:30 +0100

    Update for Bukkit's new event system

--- src/de/philworld/bukkit/minecartremover/MinecartRemover.java
+++ src/de/philworld/bukkit/minecartremover/MinecartRemover.java
@@ -2,8 +2,6 @@ package de.philworld.bukkit.minecartremover;
 
 import java.util.logging.Logger;
 
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,7 +17,7 @@ public class MinecartRemover extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvent(Event.Type.VEHICLE_EXIT, new MinecartRemoverVehicleListener(), Priority.Normal, this);
+		getServer().getPluginManager().registerEvents(new MinecartRemoverListener(), this);
 		
 		PluginDescriptionFile pff = this.getDescription();
         log.info(pff.getName() +  " " + pff.getVersion() + " is enabled.");
---
+++ src/de/philworld/bukkit/minecartremover/MinecartRemoverListener.java
@@ -0,0 +1,21 @@
+package de.philworld.bukkit.minecartremover;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class MinecartRemoverListener implements Listener {
+	public void onVehicleExit(VehicleExitEvent event) {
+		event.getVehicle().remove();
+		if(event.getExited() instanceof Player) {
+			Player p = (Player) event.getExited();
+			if(p.getGameMode() == GameMode.CREATIVE) { return; }
+			p.getInventory().addItem(new ItemStack(Material.MINECART, 1));
+			p.sendMessage(ChatColor.GREEN + "[MinecartRemover] The minecart has been added to your inventory.");
+		}
+	}
+}
